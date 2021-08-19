6f9d46d7eab979a8656b62445c227223753dd261
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-08-19 14:36:22 -0700

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var taskItemEl = document.createElement("li");
+    taskItemEl.textContent = "Wendy's new task";
+    taskItemEl.className ="task-item";
+    tasksToDoEl.appendChild(taskItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler );
+
