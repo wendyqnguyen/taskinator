75469e75430262db07444f18f956845793a6fbf8
Wendy Nguyen <wendy.nguyen@gmail.com>
2021-08-19 15:34:21 -0700

    refactor: added createTaskEl function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,37 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+var createTaskEl = function(taskDataObj) {
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create div to hold task info and add to list item
+    var taskInfoEL = document.createElement("div");
+    //give it a class name
+    taskInfoEL.className = "task-info";
+
+    // add html content to div
+    taskInfoEL.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEL);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+var taskFormHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
